08ee96dd7fce1c4c3fce4ad32bff979ec693f4b1
Douglas Rapp <drapp@stackmob.com>
2013-04-26 04:02:22 -0700

    make split char flexible

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='stackmob-parse-migrator',
-version='1.0.0',
+version='1.0.1',
 description='A script to add exported Parse data to StackMob',
 author='Douglas Rapp',
 author_email='support@stackmob.com',
